1fbc8f6dd9127acadfe9a97e64797e462bc33d1a
Alexandre Lima <alex.ferreiradelima@proton.me>
2026-06-26 23:29:49 -0300

    🐛 Fix import exception

--- src/input/mappings/__init__.py
+++ src/input/mappings/__init__.py
@@ -1,12 +1,11 @@
-from .game_action.gamepad_action_map import GAMEPAD_ACTION_MAP
-from .game_action.desktop_action_map import DESKTOP_ACTION_MAP
-
 try:
+    from .game_action.gamepad_action_map import GAMEPAD_ACTION_MAP
     from .gamepads.vgamepad_map import VGAMEPAD_KEY_MAP
 except Exception:
     pass
 
 try:
+    from .game_action.desktop_action_map import DESKTOP_ACTION_MAP
     from .gamepads.evdev_map import EVDEV_KEY_MAP
 except Exception:
     pass
